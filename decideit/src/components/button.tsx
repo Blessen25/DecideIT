5bc75fd4289b05dd5a2af392cf2025c795a93a28
Blessen25 <blessenvinoymathew25@gmail.com>
2026-03-15 17:03:28 +1100

    Changes in DecideIT website

--- decideit/src/components/button.tsx
+++ decideit/src/components/button.tsx
@@ -1,12 +1,13 @@
+import type { ButtonProps } from '../interface';
 import './styles.css';
 
-export const Button_Primary = () => {
+export const Button_Primary:React.FC<ButtonProps> = (props) => {
 
     return(
 
         <>
             <a href="#" className="button body-text button-primary fw-700 deco-none">
-                Click me 
+                {props.label}
             </a>
         </>
     )
--- decideit/src/interface.tsx
+++ decideit/src/interface.tsx
@@ -2,7 +2,16 @@ export interface ContainerProps {
 
     children: React.ReactNode;
     layout_class ?: string;
-    layout_style ?: string;
+    layout_style ?: React.CSSProperties;
     container_class ?: string;
-    container_style ?: string;
+    container_style ?: React.CSSProperties;
+}
+
+export interface ButtonProps {
+
+    label?:string;
+    icon?:string;
+    class?: string;
+    style?: React.CSSProperties;
+    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
 }
