40a7fd44857049bf0f93aa660898ad378b474740
Blessen25 <blessenvinoymathew25@gmail.com>
2026-03-15 11:40:34 +1100

    Changes in DecideIT website

--- decideit/src/components/Header/header.tsx
+++ decideit/src/components/Header/header.tsx
@@ -6,8 +6,19 @@ export const Header_Signup = () => {
     return(
 
         <>
-            <Container>
+            <Container layout_class="layout-header" container_class="container-header">
                 <div className="header">
+                    <div className="logo">
+                        <a href="#" className="fw-700 h4 header-title">Decide<span className="header-title-it">IT</span></a>
+                    </div>
+                    <div className="header-details">
+                        <a href="#">Home</a>
+                        <a href="#">About</a>
+                    </div>
+                    <div className="header-register">
+                        <a href="#">Register</a>
+                        
+                    </div>
                 </div>
             </Container>
         </>
