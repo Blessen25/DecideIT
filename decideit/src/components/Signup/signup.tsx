44931737eb8533d73e6e62093f2a7cba29b941aa
Blessen25 <blessenvinoymathew25@gmail.com>
2026-03-22 00:47:52 +1100

    Changes in DecideIT website

--- decideit/src/components/Signup/signup.tsx
+++ decideit/src/components/Signup/signup.tsx
@@ -1,3 +1,4 @@
+import { Button_Primary, Button_Primary_with_Icon, Button_Secondary_with_Icon } from '../button';
 import Container from '../layout';
 import './signup.css';
 
@@ -15,32 +16,40 @@ const Signup = () => {
                         </div>
                         <div className="signupseconddiv">
                             <form action="#" className='signupforms'>
-                                <div className="signup-form-twoforms">
-                                    <div className="signup-form-oneforms">
+                                <div className="twoforms">
+                                    <div className="oneforms">
                                         <label htmlFor="text" className='fw-600 body-text'>First Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your First Name' className='input-field-cstm' maxLength={30}/>
+                                        <input type="text" id="text" name="text" placeholder='Enter your First Name' className='input-field-cstm body-small' maxLength={30}/>
                                     </div>
-                                    <div className="signup-form-oneforms">
+                                    <div className="oneforms">
                                         <label htmlFor="text" className='fw-600 body-text'>Last Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your Last Name' className='input-field-cstm' maxLength={30}/>
+                                        <input type="text" id="text" name="text" placeholder='Enter your Last Name' className='input-field-cstm body-small' maxLength={30}/>
                                     </div>
                                 </div>
-                                <div className="signup-form-oneforms">
+                                <div className="oneforms">
                                     <label htmlFor="email" className='fw-600 body-text'>Email</label>
-                                    <input type="email" id="email" name="email" placeholder='Enter your Email' className='input-field-cstm' maxLength={80}/>
+                                    <input type="email" id="email" name="email" placeholder='Enter your Email' className='input-field-cstm body-small' maxLength={80}/>
                                 </div>
-                                <div className="signup-form-twoforms">
-                                    <div className="signup-form-oneforms">
+                                <div className="twoforms">
+                                    <div className="oneforms">
                                         <label htmlFor="password" className='fw-600 body-text'>Password</label>
-                                        <input type="password" id="password_1" name="password" placeholder='Enter your Password' className='input-field-cstm' maxLength={20}/>
+                                        <input type="password" id="password_1" name="password" placeholder='Enter your Password' className='input-field-cstm body-small' maxLength={20}/>
                                     </div>
-                                    <div className="signup-form-oneforms">
+                                    <div className="oneforms">
                                         <label htmlFor="password" className='fw-600 body-text'>Confirm Password</label>
-                                        <input type="password" id="password_2" name="password" placeholder='Confirm your Password' className='input-field-cstm' maxLength={20}/>
+                                        <input type="password" id="password_2" name="password" placeholder='Confirm your Password' className='input-field-cstm body-small' maxLength={20}/>
                                     </div>
                                 </div>
-                                <div className="signup-form-oneforms">
-                                    
+
+                                <Button_Primary label='Sign Up'/>
+                                <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+                                    <p className='body-text fw-600'>OR</p>
+                                </div>
+                                
+                                <Button_Secondary_with_Icon label='Sign Up with Google' icon='fa-brands fa-google'/>
+                                <Button_Primary_with_Icon label='Sign Up with Facebook' icon='fa-brands fa-facebook'/>
+                                <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+                                    <p className='body-text fw-600'>Already have an account?<span><a href="#">Login</a></span></p>
                                 </div>
                             </form>
                         </div>
