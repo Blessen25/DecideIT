69f0440ac49ad282deae5750b5e1306b6c090d15
Blessen25 <blessenvinoymathew25@gmail.com>
2026-03-21 17:27:34 +1100

    Changes in DecideIT website

--- decideit/src/components/Signup/signup.tsx
+++ decideit/src/components/Signup/signup.tsx
@@ -14,16 +14,33 @@ const Signup = () => {
                             <p className='body-text fw-600 ' style={{color:"#111827"}}>Sign up to create polls, invite friends, and make quick decisions.</p>
                         </div>
                         <div className="signupseconddiv">
-                            <form action="#" >
+                            <form action="#" className='signupforms'>
                                 <div className="signup-form-twoforms">
                                     <div className="signup-form-oneforms">
                                         <label htmlFor="text" className='fw-600 body-text'>First Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your First Name' className='input-field-cstm'/>
+                                        <input type="text" id="text" name="text" placeholder='Enter your First Name' className='input-field-cstm' maxLength={30}/>
                                     </div>
                                     <div className="signup-form-oneforms">
-                                        <label htmlFor="text" className='fw-600 body-text'>Sur Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your Sur Name' />
+                                        <label htmlFor="text" className='fw-600 body-text'>Last Name</label>
+                                        <input type="text" id="text" name="text" placeholder='Enter your Last Name' className='input-field-cstm' maxLength={30}/>
+                                    </div>
                                 </div>
+                                <div className="signup-form-oneforms">
+                                    <label htmlFor="email" className='fw-600 body-text'>Email</label>
+                                    <input type="email" id="email" name="email" placeholder='Enter your Email' className='input-field-cstm' maxLength={80}/>
+                                </div>
+                                <div className="signup-form-twoforms">
+                                    <div className="signup-form-oneforms">
+                                        <label htmlFor="password" className='fw-600 body-text'>Password</label>
+                                        <input type="password" id="password_1" name="password" placeholder='Enter your Password' className='input-field-cstm' maxLength={20}/>
+                                    </div>
+                                    <div className="signup-form-oneforms">
+                                        <label htmlFor="password" className='fw-600 body-text'>Confirm Password</label>
+                                        <input type="password" id="password_2" name="password" placeholder='Confirm your Password' className='input-field-cstm' maxLength={20}/>
+                                    </div>
+                                </div>
+                                <div className="signup-form-oneforms">
+                                    
                                 </div>
                             </form>
                         </div>
