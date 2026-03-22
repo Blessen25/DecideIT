0bc6e265dba0e273241156fd97d47287a461bdf1
Blessen25 <blessenvinoymathew25@gmail.com>
2026-03-22 16:53:55 +1100

    Changes in DecideIT website

--- decideit/src/components/Signup/signup2.tsx
+++ decideit/src/components/Signup/signup2.tsx
@@ -1,11 +1,14 @@
 import { Button_Primary } from '../button';
 import Container from '../layout';
 import './signup.css';
+import "react-phone-input-2/lib/style.css";
+import { useState } from 'react';
 
 const Signup_Step2 = () => {
 
+    const [preview, setPreview] = useState<string | null>(null);
     return( 
-
+        
         <>
             <Container layout_class='main_layout_login'>
                 <>
@@ -18,30 +21,49 @@ const Signup_Step2 = () => {
                             <form action="#" className='signupforms'>
                                 <div className="twoforms">
                                     <div className="oneforms">
-                                        <label htmlFor="text" className='fw-600 body-text'>First Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your First Name' className='input-field-cstm body-small' maxLength={30}/>
+                                        <label htmlFor="text" className='fw-600 body-text'>Date of Birth <span style={{color:"red"}} className='body-text'>*</span></label>
+                                        <div className="birthday-cstm">
+                                            <input type="text" id="Day" name="Day" placeholder='DD' className='input-field-cstm body-small birthday2-cstm' maxLength={2}/>
+                                            <input type="text" id="Day" name="Day" placeholder='MM' className='input-field-cstm body-small birthday2-cstm' maxLength={2}/>
+                                            <input type="text" id="Day" name="Day" placeholder='YYYY' className='input-field-cstm body-small birthday2-cstm' maxLength={4}/>
+                                        </div>
                                     </div>
                                     <div className="oneforms">
-                                        <label htmlFor="text" className='fw-600 body-text'>Last Name</label>
-                                        <input type="text" id="text" name="text" placeholder='Enter your Last Name' className='input-field-cstm body-small' maxLength={30}/>
+                                        <label htmlFor="text" className='fw-600 body-text'>User Name</label>
+                                        <input type="text" id="text" name="text" placeholder='Enter your Preferred User Name' className='input-field-cstm body-small' maxLength={30}/>
                                     </div>
-                                </div>
-                                <div className="oneforms">
-                                    <label htmlFor="email" className='fw-600 body-text'>Email</label>
-                                    <input type="email" id="email" name="email" placeholder='Enter your Email' className='input-field-cstm body-small' maxLength={80}/>
                                 </div>
                                 <div className="twoforms">
                                     <div className="oneforms">
-                                        <label htmlFor="password" className='fw-600 body-text'>Password</label>
-                                        <input type="password" id="password_1" name="password" placeholder='Enter your Password' className='input-field-cstm body-small' maxLength={20}/>
+                                        <label className='fw-600 body-text'>
+                                            Profile Picture
+                                        </label>
+                                        <div className="profile-preview">
+                                            {preview ? (
+                                            <img src={preview} alt="Preview" className="preview-img" />
+                                            ) : (
+                                            <div className="default-avatar">
+                                                <i className="fa-solid fa-user body-text"></i>
+                                            </div>
+                                            )}
+                                        </div>
+
+                                        <input
+                                            type="file"
+                                            accept="image/*"
+                                            onChange={(e) => {
+                                            const file = e.target.files[0];
+                                            if (file) {
+                                                setPreview(URL.createObjectURL(file));
+                                            }
+                                            }}
+                                        />
                                     </div>
-                                    <div className="oneforms">
-                                        <label htmlFor="password" className='fw-600 body-text'>Confirm Password</label>
-                                        <input type="password" id="password_2" name="password" placeholder='Confirm your Password' className='input-field-cstm body-small' maxLength={20}/>
-                                    </div>
+                                    
+                                    
                                 </div>
 
-                                <Button_Primary label='Confirm Details'/>
+                                <Button_Primary label='Confirm Details' route='/'/>
                             </form>
                         </div>
                     </div>
