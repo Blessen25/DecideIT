6ed5cf52b2bd58ca21e6d8c4153e4da7764d292c
Blessen25 <blessenvinoymathew25@gmail.com>
2026-04-06 10:49:48 +1000

    Changes in DecideIT website

--- decideit/src/components/Home/home.tsx
+++ decideit/src/components/Home/home.tsx
@@ -10,8 +10,8 @@ export const Home = () => {
                 <>
                     <div className="d-flex gap-resp">
                         <div className="firstdiv-home">
-                            <h1>Decide together, wherever you are</h1>
-                            <p>Whether you're choosing where to eat, what to play, or planning your next trip, DecideIT helps you create polls and make decisions with your friends instantly.</p>
+                            <h1 className='h1'>Decide together, wherever you are</h1>
+                            <p className='body-text fw-600'>Whether you're choosing where to eat, what to play, or planning your next trip, DecideIT helps you create polls and make decisions with your friends instantly.</p>
                         </div>
                         <div className="seconddiv-home">
                             <h1>Decide together, wherever you are</h1>
